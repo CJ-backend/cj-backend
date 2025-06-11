444408ff86cb955cd0af133c41ec6a29a330f194
CJY9697 <jy970114@naver.com>
2025-06-11 18:01:09 +0900

    swagger ui 오류 해결

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from rest_framework import serializers
@@ -8,6 +9,7 @@ User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(read_only=True)
     # 이메일 중복 시 에러 메시지 출력
     email = serializers.EmailField(
         validators=[
@@ -16,17 +18,37 @@ class RegisterSerializer(serializers.ModelSerializer):
             )
         ]
     )
+    nickname = serializers.CharField(
+        max_length=30,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="이미 사용 중인 닉네임입니다."
+            )
+        ],
+    )
     password = serializers.CharField(write_only=True, min_length=5)
+
+    def validate_password(self, value):
+        # Django 설정에 정의된 모든 validator 실행
+        validate_password(value)
+        return value
 
     class Meta:
         model = User
-        fields = ("email", "name", "password")  # 필요한 필드 지정
+        fields = (
+            "user_id",
+            "email",
+            "nickname",
+            "name",
+            "password",
+        )  # 필요한 필드 지정
 
     def create(self, validated_data):
         # is_active=False 상태로 유저 생성
         user = User.objects.create_user(
             email=validated_data["email"],
             password=validated_data["password"],
+            nickname=validated_data["nickname"],
             name=validated_data["name"],
         )
         # 이메일 인증 토큰 생성
